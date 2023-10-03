daed5adf9591efe7ec130b9f056ed56ab1987c89
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2023-10-03 15:22:37 +0200

    Get rid of custom githu action; Add priority to deb control

---
+++ debian/build-deb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VERSION=$1
+
+tmp_dir=.debpkg/tmp/ya-installer
+
+mkdir -p ${tmp_dir}
+mkdir -p .debpkg/DEBIAN
+
+cp -r certs/ ${tmp_dir}/certs
+cp -r whitelist/ ${tmp_dir}/whitelist
+cp -r golem-certs/ ${tmp_dir}/golem-certs
+
+cp debian/control .debpkg/DEBIAN/control
+cp debian/postinst .debpkg/DEBIAN/postinst
+chmod +x .debpkg/DEBIAN/postinst
+
+VERSION="$(echo "$VERSION" | sed -E "s,^refs/tags/,,")"
+sed -i "s/#version/${VERSION}/g" .debpkg/DEBIAN/control
+
+DEB_FILE="ya-installer-resources_${VERSION}.deb"
+
+dpkg-deb --build .debpkg "$DEB_FILE"\
+echo "deb=$DEB_FILE" >> "${GITHUB_OUTPUT}"
